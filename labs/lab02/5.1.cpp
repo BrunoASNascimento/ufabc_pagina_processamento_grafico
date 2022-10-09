5d0eee7698a0b31ea1340efb4413c17f2214ba73
matheusr42 <matheus.araujo.r42@gmail.com>
2022-10-08 22:38:53 -0300

    lab02: update 5.1

--- labs/lab02/5.1.cpp
+++ labs/lab02/5.1.cpp
@@ -3,12 +3,16 @@
  * <NomeCompleto>, RA <RA>, data DD/MM/AAAA
  */
 
-#define GL_SILENCE_DEPRECATION
-#include <iostream>
-#include <stdlib.h>
-#include <math.h>
-#include <GLUT/glut.h>
-#include <unistd.h> // needed to sleep
+#include <GL/glut.h>
+
+
+// For MAC:
+// #define GL_SILENCE_DEPRECATION
+// #include <iostream>
+// #include <stdlib.h>
+// #include <math.h>
+// #include <GLUT/glut.h>
+// #include <unistd.h> // needed to sleep
 
 /* ASCII code for the escape key. */
 #define ESCAPE 27
@@ -24,13 +28,19 @@ void display(void)
     /* draw white polygon (rectangle) with corners at
      * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
      */
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINE_LOOP);
-    glVertex3f(-0.25f, 0.5f, 0.0f);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glVertex3f(0.75f, 0.15f, 0.0f);
-    glVertex3f(0.5f, 0.75f, 0.0f);
-    glVertex3f(0.3f, 0.2f, 0.0f);
+    glBegin(GL_TRIANGLES);
+        glColor3f(.89, 1.0, 1.0);
+        glVertex3f(0.13, 0.73, 0.0); // p1
+        glVertex3f(0.26, 0.33, 0.0); // p2
+        glVertex3f(0.54, 0.61, 0.0); // p5
+
+        glVertex3f(0.26, 0.33, 0.0); // p2
+        glVertex3f(0.95, 0.42, 0.0); // p3
+        glVertex3f(0.54, 0.61, 0.0); // p5
+
+        glVertex3f(0.95, 0.42, 0.0); // p3
+        glVertex3f(0.84, 0.88, 0.0); // p4
+        glVertex3f(0.54, 0.61, 0.0); // p5
     glEnd();
 
     /* don't wait!
@@ -47,7 +57,7 @@ void init(void)
     /* initialize viewing values  */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 0.0, 0.0, 1.0, -1.0, 1.0);
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
 /*
@@ -61,9 +71,9 @@ int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(450, 450);
+    glutInitWindowSize(250, 250);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Bruno Nascimento");
+    glutCreateWindow("hello");
     init();
     glutDisplayFunc(display);
     glutMainLoop();
