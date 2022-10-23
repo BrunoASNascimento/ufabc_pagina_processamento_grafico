5d424c7c4b462eadb65b90dc1172c78bcad0e79d
BrunoASNascimento <bruno-asn@outlook.com>
2022-10-23 19:51:43 -0300

    Add title

--- labs/project/atom.cpp
+++ labs/project/atom.cpp
@@ -171,6 +171,7 @@ int main(int argc, char **argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
+    glutCreateWindow("MODELO DE BOHR");
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
     init();
