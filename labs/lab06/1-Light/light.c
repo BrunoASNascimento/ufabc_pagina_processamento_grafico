6a4de88ca69c9fb927e13fc002d6e24b6ce67340
MatheusR42 <matheus.araujo.r42@gmail.com>
2022-11-17 06:31:44 -0300

    lab06: ex 2

---
+++ labs/lab06/1-Light/light.c
@@ -0,0 +1,86 @@
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+/*  Initialize material property, light source, lighting model,
+ *  and depth buffer.
+ */
+void init(void)
+{
+   GLfloat mat_specular[] = {10.0, 1.0, 1.0, 1.0};
+   GLfloat mat_shininess[] = {80.0};
+   GLfloat light_position[] = {1.0, 1.0, 0.0, 0.0};
+   GLfloat light_position1[] = {0.0, 0.0, 1.0, 0.0};
+   GLfloat light_ambient[] = {0.2, 0.1, 0.5, 1.0};
+   GLfloat no_mat[] = {0.7, 0.5, 0.2, 1.0};
+
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+   glShadeModel(GL_SMOOTH);
+
+   glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+   glPushMatrix();
+   glLightfv(GL_LIGHT1, GL_SPECULAR, no_mat);
+   glMaterialfv(GL_FRONT, GL_AMBIENT, light_ambient);
+   glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
+   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   glPopMatrix();
+
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_LIGHT1);
+   glEnable(GL_DEPTH_TEST);
+}
+
+void display(void)
+{
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glutSolidSphere(1.0, 20, 16);
+   glFlush();
+}
+
+void reshape(int w, int h)
+{
+   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   if (w <= h)
+      glOrtho(-1.5, 1.5, -1.5 * (GLfloat)h / (GLfloat)w,
+              1.5 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
+   else
+      glOrtho(-1.5 * (GLfloat)w / (GLfloat)h,
+              1.5 * (GLfloat)w / (GLfloat)h, -1.5, 1.5, -10.0, 10.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+   switch (key)
+   {
+   case 27:
+      exit(0);
+      break;
+   }
+}
+
+int main(int argc, char **argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+   glutInitWindowSize(500, 500);
+   glutInitWindowPosition(100, 100);
+   glutCreateWindow(argv[0]);
+   init();
+   glMatrixMode(GL_MODELVIEW);
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutKeyboardFunc(keyboard);
+   glutMainLoop();
+   return 0;
+}
